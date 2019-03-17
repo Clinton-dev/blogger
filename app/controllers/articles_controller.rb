a41b8fc55599588af069dcc575ae8616438c44a0
clinton wambugu <clintonwambugu@gmail.com>
2019-03-17 23:14:44 +0300

    working on new method of the form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,16 @@ class ArticlesController < ApplicationController
         @articles = Article.all
       end
       
+    def new 
+      @article = Article.new
+    end
+
+    def create
+      @article = Article.new
+      @article =  params[:article][:title]
+      @article.save
+      redirect_to article_path(@article)
+
+    end
+    
 end
